88416ffcdfb8681e236d5caaafc6c248b1d865ec
Jan Burda <janburda03@gmail.com>
2025-07-10 00:43:46 +0200

    Delete Packing directory

--- Packing/PlacementHeuristics.cs
+++
@@ -1,7 +0,0 @@
-﻿public class PlacementHeuristics
-{
-    public (Container, Coordinates) WhereToPack(ItemToBePacked itemToBePacked, IList<Container> containers)
-    {
-        return (null, new Coordinates(0, 0, 0));
-    }
-}
