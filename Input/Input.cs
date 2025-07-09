4e065661261df27621206a56cc9990661ff3fad2
Jan Burda <janburda03@gmail.com>
2025-07-10 00:44:35 +0200

    Add files via upload

---
+++ Input/Input.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Text.Json;
+
+public record class PackingInput(ContainerProperties Container, List<ItemProperties> Items);
+
+public record class ContainerProperties(Dimensions Dimension, int MaxWeight);
+
+public record class ItemProperties(int Id, Dimensions Dimension, int Weight);
+
+public static class PackingInputLoader
+{
+    public static PackingInput LoadFromFile(string fileName)
+    {
+        string json = File.ReadAllText(fileName);
+        PackingInput? input = JsonSerializer.Deserialize<PackingInput>(json);
+        if (input == null)
+            throw new Exception("JSON deserialize error!");
+        return input;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("ahoj");
+    }
+}
+
+
---
+++ Packing/Packing.cs
@@ -0,0 +1,28 @@
+﻿class PackingInstance
+{
+    List<Container> Containers;
+
+    List<ItemToBePacked> Items;
+
+    public void PackAll()
+    {
+        Container container;
+        Coordinates coordinate;
+        PlacementHeuristics heuristics;
+
+        foreach (ItemToBePacked itemToBePacked in Items) 
+        { 
+            heuristics = itemToBePacked.Heuristics;
+            (container, coordinate) = heuristics.WhereToPack(itemToBePacked, Containers);
+            container.PackItem(itemToBePacked, coordinate);
+        }
+    }
+}
+
+
+
+
+
+
+
+
