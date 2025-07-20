a665a2136161ac75bd49dc9bb335801a495a2462
Jan Burda <janburda03@gmail.com>
2025-07-20 23:47:49 +0200

    Delete Vector directory

--- Vector/VectorDecoder.cs
+++
@@ -1,9 +0,0 @@
-﻿
-public static class X
-{
-    public static IList<BoxToBePacked> DecodeVectorToPackingSequence(IList<BoxProperties> boxesProperties)
-    {
-        
-        return (from BoxProperties boxProperties in boxesProperties select new BoxToBePacked(boxProperties, Rotation.XYZ, PlacementHeuristics.FirstFit)).ToList();
-    }
-}
