6579618b15725d9f54ba0901d374c26862abb0de
Jan Burda <janburda03@gmail.com>
2025-08-12 01:36:24 +0200

    Add files via upload

---
+++ App/Program.cs
@@ -0,0 +1,13 @@
+﻿
+class Program
+{
+    [STAThread]
+    static void Main()
+    {
+        ProgramSetting? setting = FormProgram.Run();
+        if (setting != null)
+        {
+            PackingProgram.Run((ProgramSetting)setting);
+        }
+    }
+}
