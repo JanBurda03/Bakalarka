ae792d1b7f9c6a14fdf9ccfb76ac0399ab183c06
Jan Burda <janburda03@gmail.com>
2025-08-12 01:32:59 +0200

    Delete Evolution directory

--- Evolution/Elitism/IElitism.cs
+++
@@ -1,7 +0,0 @@
-﻿public interface IElitism<T> where T : IComparable<T>
-{
-    public int NumberOfElites { get; init; }
-    public bool Minimizing { get; init; }
-    public IReadOnlyList<T> GetElites(IReadOnlyList<T> list);
-}
-
